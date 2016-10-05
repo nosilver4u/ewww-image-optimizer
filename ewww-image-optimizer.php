5e25d2c77a24d8845e75d2026d0ea5410da0620e
Shane Bishop <shanebishop@ewww.io>
2016-10-05 15:18:34 -0600

    forgot to remove nextcellent init hack

--- ewww-image-optimizer.php
+++ ewww-image-optimizer.php
@@ -111,7 +111,7 @@ function ewww_image_optimizer_exec_init() {
 		add_action( 'load-ims_gallery_page_ewww-ims-optimize', 'ewww_image_optimizer_tool_init' );
 		add_action( 'load-media_page_ewww-image-optimizer-unoptimized', 'ewww_image_optimizer_tool_init' );
 		add_action( 'load-flagallery_page_flag-manage-gallery', 'ewww_image_optimizer_tool_init' );
-		add_action( 'load-galleries_page_nggallery-manage', 'ewww_image_optimizer_tool_init' );
+//		add_action( 'load-galleries_page_nggallery-manage', 'ewww_image_optimizer_tool_init' );
 //		add_action( 'load-', 'ewww_image_optimizer_tool_init' );
 	} 
 	ewwwio_memory( __FUNCTION__ );
