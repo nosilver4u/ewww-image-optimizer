ee6ed15a77b996aed59b089ed6c67a5d1a17388d
Shane Bishop <shanebishop@ewww.io>
2019-05-02 11:49:31 -0600

    properly exclude all script tags from LL parsing

--- classes/class-ewwwio-lazy-load.php
+++ classes/class-ewwwio-lazy-load.php
@@ -153,7 +153,7 @@ class EWWWIO_Lazy_Load extends EWWWIO_Page_Parser {
 		$above_the_fold   = apply_filters( 'ewww_image_optimizer_lazy_fold', 0 );
 		$images_processed = 0;
 
-		$images = $this->get_images_from_html( preg_replace( '/<noscript.*?\/noscript>/s', '', $buffer ), false );
+		$images = $this->get_images_from_html( preg_replace( '/<(noscript|script).*?\/\1>/s', '', $buffer ), false );
 		if ( ! empty( $images[0] ) && ewww_image_optimizer_iterable( $images[0] ) ) {
 			foreach ( $images[0] as $index => $image ) {
 				$images_processed++;
@@ -345,9 +345,9 @@ class EWWWIO_Lazy_Load extends EWWWIO_Page_Parser {
 			return false;
 		}
 
-		$image_src = $this->get_attribute( $image, 'src' );
 		// Skip inline data URIs.
-		if ( false !== strpos( $image_src, 'data:image' ) || 0 === strpos( $image_src, "'" ) || false !== strpos( $image_src, '(' ) ) {
+		$image_src = $this->get_attribute( $image, 'src' );
+		if ( false !== strpos( $image_src, 'data:image' ) ) {
 			return false;
 		}
 		// Ignore 0-size Pinterest schema images.
