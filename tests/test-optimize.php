fd948148a50f2eb925a14615dc56115d50d02268
Shane Bishop <shanebishop@ewww.io>
2019-10-15 15:30:05 -0600

    fixing pngquant unit test results

--- tests/test-optimize.php
+++ tests/test-optimize.php
@@ -334,7 +334,7 @@ class EWWWIO_Optimize_Tests extends WP_UnitTestCase {
 		update_site_option( 'ewww_image_optimizer_optipng_level', 2 );
 		update_site_option( 'ewww_image_optimizer_metadata_remove', true );
 		$results = $this->optimize_png();
-		$this->assertEquals( 37428, filesize( $results[0] ) );
+		$this->assertEquals( 38639, filesize( $results[0] ) );
 		unlink( $results[0] );
 	}
 
