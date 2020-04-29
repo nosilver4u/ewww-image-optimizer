c99671ca068e5b31c516f7551c175c820d220d0a
Shane Bishop <shanebishop@ewww.io>
2020-04-29 11:10:59 -0600

    doc changes for 5.3

--- common.php
+++ common.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'EWWW_IMAGE_OPTIMIZER_VERSION', '525.216' );
+define( 'EWWW_IMAGE_OPTIMIZER_VERSION', '530' );
 
 // Initialize a couple globals.
 $eio_debug  = '';
