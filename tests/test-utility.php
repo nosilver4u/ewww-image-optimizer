ab1cdf33cbfdf1869a391439cc97a70eee48bbe0
Shane Bishop <shanebishop@ewww.io>
2021-04-12 11:00:39 -0600

    updated CF IP list

--- tests/test-utility.php
+++ tests/test-utility.php
@@ -129,9 +129,10 @@ class EWWWIO_Utility_Tests extends WP_UnitTestCase {
 			'197.234.240.0/22',
 			'198.41.128.0/17',
 			'162.158.0.0/15',
-			'104.16.0.0/12',
 			'172.64.0.0/13',
 			'131.0.72.0/22',
+			'104.16.0.0/13',
+			'104.24.0.0/14',
 		);
 		foreach( $latest_ips as $key => $range ) {
 			if ( empty( $range ) ) {
