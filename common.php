3b1a815ee777f2d44679c5b7c01b13bab28877c1
Shane Bishop <shanebishop@ewww.io>
2021-06-22 08:54:19 -0600

    forgot version bump to clear JS cache for LL

--- common.php
+++ common.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'EWWW_IMAGE_OPTIMIZER_VERSION', '617.0' );
+define( 'EWWW_IMAGE_OPTIMIZER_VERSION', '617.1' );
 
 // Initialize a couple globals.
 $eio_debug  = '';
