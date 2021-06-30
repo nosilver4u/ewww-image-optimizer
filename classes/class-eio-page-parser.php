fe09e758a173b74704164f6699bd1d3e7482dc7e
Shane Bishop <shanebishop@ewww.io>
2021-06-30 10:34:43 -0600

    fix unquoted img matching src= with no actual URL

--- classes/class-eio-page-parser.php
+++ classes/class-eio-page-parser.php
@@ -57,7 +57,7 @@ if ( ! class_exists( 'EIO_Page_Parser' ) ) {
 			} elseif ( $src_required ) {
 				$this->debug_message( 'using plain img pattern, src still required' );
 				$search_pattern   = '#(?P<img_tag><img[^>]*?\s+?src\s*=\s*("|\')(?P<img_url>(?!\2).+?)\2[^>]*?>)#is';
-				$unquoted_pattern = '#(?P<img_tag><img[^>]*?\s+?src\s*=\s*(?P<img_url>[^"\'\\\\][^\s>]+)[^>]*?>)#is';
+				$unquoted_pattern = '#(?P<img_tag><img[^>]*?\s+?src\s*=\s*(?P<img_url>[^"\'\\\\<>][^\s<>]+)[^>]*?>)#is';
 			}
 			if ( preg_match_all( $search_pattern, $content, $images ) ) {
 				$this->debug_message( 'found ' . count( $images[0] ) . ' image elements with quoted pattern' );
