563c1ee4f27ef6ed8c7de578acd317ed3ed2f118
Shane Bishop <shanebishop@ewww.io>
2021-10-14 08:25:58 -0600

    releasing 6.3

--- common.php
+++ common.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'EWWW_IMAGE_OPTIMIZER_VERSION', '625' );
+define( 'EWWW_IMAGE_OPTIMIZER_VERSION', '630' );
 
 // Initialize a couple globals.
 $eio_debug  = '';
