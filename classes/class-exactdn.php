5dbd90a63bd08e39fb6804b39e9611e47de8a4dd
Shane Bishop <shanebishop@ewww.io>
2022-01-06 14:35:27 -0700

    tighten up regex for all the things
    The catch all regex was catching things it ought not to. Now the path prefix must be comprised of valid folder sub-parts, and those sub-parts must not contain /, :, and &. The latter was in the debug line, but not in the actual regex for some (unknown) reason.

--- classes/class-exactdn.php
+++ classes/class-exactdn.php
@@ -1805,8 +1805,8 @@ if ( ! class_exists( 'ExactDN' ) ) {
 					$this->debug_message( 'searching for #(https?:)?//(?:www\.)?' . $escaped_upload_domain . $this->remove_path . '/#i and replacing with $1//' . $this->exactdn_domain . '/' );
 					$content = preg_replace( '#(https?:)?//(?:www\.)?' . $escaped_upload_domain . $this->remove_path . '/#i', '$1//' . $this->exactdn_domain . '/', $content );
 				} else {
-					$this->debug_message( 'searching for #(https?:)?//(?:www\.)?' . $escaped_upload_domain . '/([^"\'?&>]+?)?(nextgen-image|' . $this->include_path . '|' . $this->content_path . ')/#i and replacing with $1//' . $this->exactdn_domain . '/$2$3/' );
-					$content = preg_replace( '#(https?:)?//(?:www\.)?' . $escaped_upload_domain . '/([^"\'?>]+?)?(nextgen-image|' . $this->include_path . '|' . $this->content_path . ')/#i', '$1//' . $this->exactdn_domain . '/$2$3/', $content );
+					$this->debug_message( 'searching for #(https?:)?//(?:www\.)?' . $escaped_upload_domain . '(/[^"\'?&>:/]+?)*?/(nextgen-image|' . $this->include_path . '|' . $this->content_path . ')/#i and replacing with $1//' . $this->exactdn_domain . '$2/$3/' );
+					$content = preg_replace( '#(https?:)?//(?:www\.)?' . $escaped_upload_domain . '(/[^"\'?&>:/]+?)*?/(nextgen-image|' . $this->include_path . '|' . $this->content_path . ')/#i', '$1//' . $this->exactdn_domain . '$2/$3/', $content );
 				}
 				$content = str_replace( '?wpcontent-bypass?', $this->content_path, $content );
 			}
--- readme.txt
+++ readme.txt
@@ -148,6 +148,7 @@ That's not a question, but since I made it up, I'll answer it. See this resource
 * fixed: error when disabling Easy IO
 * fixed: Easy IO misses some image URLs on multi-site when using domain-mapping
 * fixed: SVG level cannot be set when using API if svgcleaner was not installed previously
+* fixed: Easy IO URL rewriter changing links if they matched a custom upload folder
 
 = 6.3.0 =
 * added: EIO_LAZY_FOLD override to configure number of images above-the-fold that will be skipped by Lazy Load
