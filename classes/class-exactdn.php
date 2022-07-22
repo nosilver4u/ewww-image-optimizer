6560d3c95d716e296b37869532d831f75aca8e7f
Shane Bishop <shanebishop@ewww.io>
2022-07-22 08:43:33 -0600

    add built-in AJAX exclusion for custom plugin hooks

--- classes/class-exactdn.php
+++ classes/class-exactdn.php
@@ -230,6 +230,7 @@ if ( ! class_exists( 'ExactDN' ) ) {
 			// Overrides for admin-ajax images.
 			add_filter( 'exactdn_admin_allow_image_downsize', array( $this, 'allow_admin_image_downsize' ), 10, 2 );
 			add_filter( 'exactdn_admin_allow_image_srcset', array( $this, 'allow_admin_image_downsize' ), 10, 2 );
+			add_filter( 'exactdn_admin_allow_plugin_url', array( $this, 'allow_admin_image_downsize' ), 10, 2 );
 			// Overrides for "pass through" images.
 			add_filter( 'exactdn_pre_args', array( $this, 'exactdn_remove_args' ), 10, 3 );
 			// Overrides for user exclusions.
